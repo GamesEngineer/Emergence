0e87f42b11f9f5f2e46c976f0171293f9c5a8e85
Jason Conaway <conaway4893@gmail.com>
2022-09-06 18:36:35 -0400

    Added instructions and option to quit

--- Assets/Scripts/RulesPanel.cs
+++ Assets/Scripts/RulesPanel.cs
@@ -2,24 +2,24 @@ using UnityEngine;
 
 public class RulesPanel : MonoBehaviour
 {
-    private bool isPanelOpen;
+    private bool isPanelOpen = true;
+
+    private void Start()
+    {
+        SetPanelState(isPanelOpen);
+    }
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Quit();
+        }
+
         // Show/hide the panel when the Space key is pressed
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            isPanelOpen = !isPanelOpen;
-            Time.timeScale = isPanelOpen ? 0.05f : 1f; // slow time when the panel is open
-
-            if (!isPanelOpen)
-            {
-                var rs = GetComponentsInChildren<RuleInput>();
-                foreach (var r in rs)
-                {
-                    r.CommitChanges();
-                }
-            }
+            SetPanelState(!isPanelOpen);
         }
 
         // Randomize the rules when the R key is pressed
@@ -35,8 +35,32 @@ public class RulesPanel : MonoBehaviour
         }
         
         // Slide the panel up/down in response to its open/closed state
-        var p = transform.localPosition;
-        p.y = Mathf.MoveTowards(p.y, isPanelOpen ? 0f : -945f, Time.unscaledDeltaTime * 3000f);
-        transform.localPosition = p;
+        var p = transform.parent.localPosition;
+        p.y = Mathf.MoveTowards(p.y, isPanelOpen ? 0f : -1050f, Time.unscaledDeltaTime * 3000f);
+        transform.parent.localPosition = p;
+    }
+
+    private void SetPanelState(bool openPanel)
+    {
+        isPanelOpen = openPanel;
+        Time.timeScale = isPanelOpen ? 0.05f : 1f; // slow time when the panel is open
+
+        if (!isPanelOpen)
+        {
+            var rules = GetComponentsInChildren<RuleInput>();
+            foreach (var rule in rules)
+            {
+                rule.CommitChanges();
+            }
+        }
+    }
+
+    private void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 }
